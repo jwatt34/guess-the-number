5dcf9963cbbcd4f900e072d886f573b1b71551a5
jdwatt <jdwatt@aggies.ncat.edu>
2022-03-28 12:14:12 -0400

    refactor using updateMessage function and remove print statements

--- static/script.js
+++ static/script.js
@@ -3,29 +3,31 @@
 let number = Math.trunc(Math.random() * 19) + 1;
 let score = 20;
 let highscore = 0;
-console.log(number)
+//change the displayed message
+const updateMessage = function(message){
+  document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function(){
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
   if (score > 1){
     //blank guess
     if (!guess){
-      document.querySelector('.message').textContent = '😒 Please Guess a Number';
+      updateMessage('😒 Please Guess a Number');
     }
     //guess is incorrect
     else if (guess != number){
       score --;
       let message = guess > number ? 'Too High, Guess Again' : "Too Low, Guess Again";
-      document.querySelector('.message').textContent = `${message}`;
+      updateMessage(message);
       document.querySelector('.score').textContent = score;
     }
       //guess is correct
     else{
-      document.querySelector('.message').textContent = '🎉 Correct Number!';
+      updateMessage('🎉 Correct Number!');
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').textContent = number;
-      document.querySelector('.number').style.width = '30rem'
+      document.querySelector('.number').style.width = '30rem';
       if (score > highscore){
         highscore = score;
         document.querySelector('.highscore').textContent = highscore;
@@ -34,17 +36,17 @@ document.querySelector('.check').addEventListener('click', function(){
     }
     //score reaches 0
     else{
-      document.querySelector('.message').textContent = 'You lose'
+      updateMessage('You lose');
     }
 })
 
 document.querySelector('.again').addEventListener('click', function(){
-  number = Math.trunc(Math.random() * 20)
+  number = Math.trunc(Math.random() * 20);
   score = 20;
   console.log(`answer = ${number}`);
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  updateMessage('Start guessing...');
   document.querySelector('body').style.backgroundColor = 'black';
   document.querySelector('.score').textContent = score;
 })
